47b861b3f82edaf2fbac67706f6bc5716108dcef
Patryk Wertka <spartanpageagain@gmail.com>
2016-02-05 23:49:03 +0100

    Added walls loading

--- app/game/core/layers/load.rb
+++ app/game/core/layers/load.rb
@@ -15,4 +15,14 @@ module Tiles
       ]
     }]
   end
+
+  def self.load_walls(dirpath)
+    data_hash = load_tiles_json(dirpath+'/walls.json')
+    name_binded_tile_images = Hash[data_hash['tiles'].map{
+        |wall_name, wall_data| [
+          wall_name.intern,
+          [Gosu::Image.new(dirpath+'/'+wall_data['image_path'], :tileable => true)]
+      ]
+    }]
+  end
 end
--- app/game/core/layers/tile_data.rb
+++ app/game/core/layers/tile_data.rb
@@ -1,16 +1,16 @@
 class TileData
   class TileDataElement
     attr_reader :symbol, :image, :is_solid
-    def initialize(sym, image, is_solid)
+    def initialize(sym, data)
       @symbol = sym
-      @image = image
-      @is_solid = is_solid
+      @image = data[0]
+      @is_solid = data[1]
     end
   end
   attr_accessor :tiles, :ids
   def initialize(tiles)
     @tiles = tiles
 
-    @ids = tiles.map{ |k, data| TileDataElement.new(k, data[0], data[1]) }
+    @ids = tiles.map{ |k, data| TileDataElement.new(k, data) }
   end
 end

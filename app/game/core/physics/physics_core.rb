d290670b983c4572ca4d9fcff99bc445d225e784
Patryk Wertka <spartanpageagain@gmail.com>
2016-02-05 18:03:53 +0100

    Added proper gravitation, falling, simulation substeps and space.

--- app/game/core/physics/physics_core.rb
+++ app/game/core/physics/physics_core.rb
@@ -1,6 +1,13 @@
 require './game/core/world/block'
 class PhysicsCore
-  def initialize; end
+  GRAVITY = 0.0001
+  SUBSTEPS = 5
+
+  attr_accessor :space
+  def initialize
+    @space = CP::Space.new
+    @space.gravity = CP::Vec2.new(0, GRAVITY)
+  end
 
   def zero_falling_of(body)
     body.v.y = 0 if body.v.y > 0
@@ -10,18 +17,22 @@ class PhysicsCore
     trans_pos_x = (body.p.x/Block::width).floor
     trans_pos_y = (body.p.y/Block::height).floor
     return false if trans_pos_x < 0 || trans_pos_y < 0 || trans_pos_x >= @world.width || trans_pos_y >= @world.height
-    iss = @tile_data.ids[@world.block_at(trans_pos_x, trans_pos_y).id].is_solid
-    print iss if iss
-    iss
+    @tile_data.ids[@world.block_at(trans_pos_x, trans_pos_y).id].is_solid
   end
 
   def update(world, tile_data, actors, dt)
     @world = world
     @tile_data = tile_data
 
-    actors.each do |actor|
-      body = actor.physical_attributes.body
-      zero_falling_of body if touching_ground? body
+    SUBSTEPS.times do
+      actors.each do |actor|
+        attrs = actor.physical_attributes
+        body = attrs.body
+        zero_falling_of body if touching_ground? body
+
+        attrs.shape.body.reset_forces
+      end
+      @space.step(dt)
     end
   end
 end

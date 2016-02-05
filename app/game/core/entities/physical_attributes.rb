13d63f89f192e0b0485f1d689e23a448a2a6eba5
Patryk Wertka <spartanpageagain@gmail.com>
2016-02-05 18:03:29 +0100

    Took care of shape and space in physical attributes

--- app/game/core/entities/physical_attributes.rb
+++ app/game/core/entities/physical_attributes.rb
@@ -1,9 +1,18 @@
 require 'chipmunk'
 
+def quad_shape(body, width, height, offset = CP::Vec2.new(0,0))
+  shape_verts = [CP::Vec2.new(-width, height), CP::Vec2.new(width, height),
+                 CP::Vec2.new(width, -height), CP::Vec2.new(-width, -height), ]
+  CP::Shape::Poly.new(body, shape_verts, offset)
+end
+
 class PhysicalAttributes
   attr_accessor :body, :shape
-  def initialize(physical_body, physical_shape)
+  def initialize(space, physical_body, physical_shape)
     @body = physical_body
     @shape = physical_shape
+
+    space.add_body(@body)
+    space.add_shape(@shape)
   end
 end

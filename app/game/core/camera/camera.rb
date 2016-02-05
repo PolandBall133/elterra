a8ca4bc3f6690ca2c7c3d94fd7e4b860fffb8d5b
Patryk Wertka <spartanpageagain@gmail.com>
2016-02-05 15:28:19 +0100

    Camera uses focused physic body for x, y

--- app/game/core/camera/camera.rb
+++ app/game/core/camera/camera.rb
@@ -1,17 +1,28 @@
 class Camera
-  attr_accessor :position_x, :position_y
   def initialize(world, tile_data)
     @world = world
     @tile_data = tile_data
     @position_x = @position_y = 0
   end
 
-  def draw(zorder, screen_width, screen_height)
+  def set_viewport(width, height)
+    @viewport_width = width
+    @viewport_height = height
+  end
+
+  def focus(physic_body)
+    @focused_body = physic_body
+  end
+
+  def draw_tiles(zorder)
+    position_x = @focused_body.p.x.floor
+    position_y = @focused_body.p.y.floor
+
     offset_x = position_x % Block::width
     offset_y = position_y % Block::height
 
-    (0..screen_width).step(Block::width).each do |x|
-      (0..screen_height).step(Block::height).each do |y|
+    (0..@viewport_width).step(Block::width).each do |x|
+      (0..@viewport_height).step(Block::height).each do |y|
         trans_x = x+position_x
         trans_y = y+position_y
         next if trans_x < 0 || trans_y < 0 || trans_x >= @world.width*Block::width || trans_y >= @world.height*Block::height
@@ -22,6 +33,12 @@ class Camera
 
         @tile_data.ids[block.id].image.draw(block_x, block_y, zorder)
       end
+    end
+  end
+
+  def draw_actors(actors)
+    actors.each do |actor|
+      actor.draw(@viewport_width/2, @viewport_height/2)
     end
   end
 end

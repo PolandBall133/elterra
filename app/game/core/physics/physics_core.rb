c4d919fa513b985c156d5fb857e08a2cc9016bb4
Patryk Wertka <spartanpageagain@gmail.com>
2016-02-05 19:13:08 +0100

    Added horizontal collision of actors with blocks, fixed actor "burrowing in ground" after hard landing

--- app/game/core/physics/physics_core.rb
+++ app/game/core/physics/physics_core.rb
@@ -10,14 +10,41 @@ class PhysicsCore
   end
 
   def zero_falling_of(body)
-    body.v.y = 0 if body.v.y > 0
+    if body.v.y > 0
+      body.p.y -= body.p.y.floor % Block::height
+      body.v.y = 0
+    end
+  end
+
+  def test_solid(x, y)
+    trans_pos_x = (x/Block::width).floor
+    trans_pos_y = (y/Block::height).floor
+    return unless @world.in_bounds? trans_pos_x, trans_pos_y
+    @tile_data.ids[@world.block_at(trans_pos_x, trans_pos_y).id].is_solid
   end
 
   def touching_ground?(body)
-    trans_pos_x = (body.p.x/Block::width).floor
-    trans_pos_y = (body.p.y/Block::height).floor
-    return unless @world.in_bounds? trans_pos_x, trans_pos_y
-    @tile_data.ids[@world.block_at(trans_pos_x, trans_pos_y).id].is_solid
+    test_solid(body.p.x, body.p.y)
+  end
+
+  def zero_left_movement_of(body)
+    if(body.v.x < 0)
+      body.v.x = 0.0
+    end
+  end
+
+  def zero_right_movement_of(body)
+    if(body.v.x > 0)
+      body.v.x = 0.0
+    end
+  end
+
+  def touching_left?(body, width)
+    test_solid(body.p.x-1, body.p.y-1)
+  end
+
+  def touching_right?(body, width)
+    test_solid(body.p.x+1, body.p.y-1)
   end
 
   def update(world, tile_data, actors, dt)
@@ -29,7 +56,8 @@ class PhysicsCore
         attrs = actor.physical_attributes
         body = attrs.body
         zero_falling_of body if touching_ground? body
-
+        zero_left_movement_of body if touching_left? body, actor.width
+        zero_right_movement_of body if touching_right? body, actor.width
         attrs.shape.body.reset_forces
       end
       @space.step(dt)

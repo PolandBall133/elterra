2e2bcac2141a793f3ae968597e73b77e6e70f426
Patryk Wertka <spartanpageagain@gmail.com>
2016-02-07 17:08:19 +0100

    Tested game_world.in_bounds?

--- app/game/core/world/game_world_test.rb
+++ app/game/core/world/game_world_test.rb
@@ -1,9 +1,32 @@
 require 'minitest/autorun'
 
 require_relative 'game_world'
-require_relative 'block'
-require_relative 'wall'
 
 describe GameWorld do
-  #todo; also: tests for static_layers
+  before do
+    @world = GameWorld.new(2, 2, nil, nil)
+  end
+
+  describe 'GameWorld bounds check' do
+    it 'first element is in bounds' do
+      assert @world.in_bounds? 0, 0
+    end
+
+    it 'elements before the first elements are not in bounds' do
+      assert_equal false, @world.in_bounds?(0, -1)
+      assert_equal false, @world.in_bounds?(-1, 0)
+      assert_equal false, @world.in_bounds?(-1, -1)
+    end
+
+    it 'last element is in bounds' do
+      assert @world.in_bounds? @world.width-1, @world.height-1
+    end
+
+    it 'elements after the last element are not in bounds' do
+      max_x, max_y = @world.width-1, @world.height-1
+      assert_equal false, @world.in_bounds?(max_x, max_y+1)
+      assert_equal false, @world.in_bounds?(max_x+1, max_y)
+      assert_equal false, @world.in_bounds?(max_x+1, max_y+1)
+    end
+  end
 end
